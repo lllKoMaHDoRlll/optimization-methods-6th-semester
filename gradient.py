0eed6cd25362b4a391b0c8918e6c527a6a162077
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2025-04-18 21:00:43 +0300

    complete gradient method

--- gradient.py
+++ gradient.py
@@ -1,20 +1,23 @@
 import math
 
 def func(x: list[float]) -> float:
-    return 2 * x[0] * x[0] + x[0] * x[1] + x[1]*x[1]
+    return x[0]*x[0] + 5 * x[1] * x[1] + x[0] * x[1] + x[0]
 
-def find_min_t(x: list[float], grad: list[float] = [0, 0]) -> float: # переделать с градом
-    return (
-        (4 * x[0] + x[1])**2 + (x[0] + 2 * x[1])**2
+def find_min_t(x: list[float], grad_value: list[float]) -> float:
+    return  (
+        2 * x[0] * grad_value[0] + 10 * x[1] * grad_value[1] + x[0] * grad_value[1] + x[1] * grad_value[0] + grad_value[0]
     ) / (
-        4 * (4 * x[0] + x[1])**2 + 2 * (4 * x[0] + x[1]) * (x[0] + 2 * x[1]) + 2 * (x[0] + 2 * x[1])**2
+        2 * grad_value[0] * grad_value[0] + 10 * grad_value[1] * grad_value[1] + 2 * grad_value[0] * grad_value[1]
     )
 
-def partial(i: int, x: list[float]) -> list[float]:
+def grad(x: list[float]) -> list[float]:
+    grad = [0. for _ in range(len(x))]
     epsilon = 0.0001
-    x_ = x[:]
-    x_[i] += epsilon
-    return (func(x_) - func(x)) / epsilon
+    for i in range(len(x)):
+        x_ = x[:]
+        x_[i] += epsilon
+        grad[i] =  (func(x_) - func(x)) / epsilon
+    return grad
 
 def norm(vec: list[float]) -> float:
     return math.sqrt(sum([el * el for el in vec]))
@@ -29,10 +32,10 @@ def gradient_decrease(x0: list[float], epsilon1: float, epsilon2: float, M: int)
     while True:
         print(f"{k=}")
         print(f"{x_prev}")
-        grad = [partial(i, x_prev) for i in range(len(x0))]
-        print(f"{grad=}")
-        print(f"norm_grad: {norm(grad)}")
-        if norm(grad) < epsilon1:
+        grad_value = grad(x_prev)
+        print(f"{grad_value=}")
+        print(f"norm_grad: {norm(grad_value)}")
+        if norm(grad_value) < epsilon1:
             x_min = x_prev
             condition = "1"
             break
@@ -40,10 +43,9 @@ def gradient_decrease(x0: list[float], epsilon1: float, epsilon2: float, M: int)
             x_min = x_prev
             condition = "2"
             break
-        # t = find_min_t(x_prev, grad)
-        t = find_min_t(x_prev)
+        t = find_min_t(x_prev, grad_value)
         print(f"{t=}")
-        x_next = [x_prev[i] - t * grad[i] for i in range(len(x0))]
+        x_next = [x_prev[i] - t * grad_value[i] for i in range(len(x0))]
         print(f"{x_next=}")
         print(f"norm: {norm([x_next[i] - x_prev[i] for i in range(len(x0))])}, delta: {abs(func(x_next) - func(x_prev))}")
         if norm([x_next[i] - x_prev[i] for i in range(len(x0))]) < epsilon2 and abs(func(x_next) - func(x_prev)) < epsilon2:
@@ -65,4 +67,4 @@ def gradient_decrease(x0: list[float], epsilon1: float, epsilon2: float, M: int)
         "condition": condition
     }
 
-print(gradient_decrease([0.5, 1], 0.1, 0.15, 10))
+print(gradient_decrease([1, 1], 0.1, 0.15, 10))
